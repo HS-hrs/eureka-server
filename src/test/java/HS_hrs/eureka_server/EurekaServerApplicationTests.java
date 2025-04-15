32ebf4137a2340f535d8422f3bb3af6cc4c51fa7
hyunwoo <kshsim98@naver.com>
2025-04-15 11:10:04 +0900

    /

---
+++ src/test/java/HS_hrs/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package HS_hrs.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
